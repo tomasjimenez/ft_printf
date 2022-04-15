96861ae1e18e682d04d831bf8026f6c85791017f
Tomas Jimenez <tomas.jimenez.flores@gmail.com>
2022-04-15 22:03:23 +0200

    ft_printf__final

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: tojimene <tojimene@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 18:21:50 by tojimene          #+#    #+#             */
-/*   Updated: 2022/04/15 21:46:05 by tojimene         ###   ########.fr       */
+/*   Updated: 2022/04/15 22:00:04 by tojimene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	print_char(char str, int *len)
 
 int	print_str(char	*str, int *len)
 {
-	if (!*str)
+	if (!str)
 		str = "(null)";
 	while (*str)
 		*len += write(1, str++, 1);
